f5d33008060d0541b679478721d5c4a8ca7786ff
Settius <settius@gmail.com>
2024-09-09 08:22:04 +0200

    - 5.4 update

--- Source/TTToolbox/Private/TTCopyAllCurvesAnimModifier.cpp
+++ Source/TTToolbox/Private/TTCopyAllCurvesAnimModifier.cpp
@@ -25,42 +25,34 @@
 
 void UTTCopyAllCurvesAnimModifier::OnApply_Implementation(UAnimSequence* TargetSequence)
 {
-  // check input arguments
-  if (!IsValid(TargetSequence) || !IsValid(SourceSequence))
-  {
-    UE_LOG(LogTemp, Error, TEXT("Called \"OnApply_Implementation\" without valid \"TargetSequence\" or valid \"SourceSequence\"."));
-    return;
-  }
+	// check input arguments
+	if (!IsValid(TargetSequence) || !IsValid(SourceSequence))
+	{
+		UE_LOG(LogTemp, Error, TEXT("Called \"OnApply_Implementation\" without valid \"TargetSequence\" or valid \"SourceSequence\"."));
+		return;
+	}
 
-  if (ReplaceExistingCurves)
-  {
-    UAnimationBlueprintLibrary::RemoveAllCurveData(TargetSequence);
-  }
+	if (ReplaceExistingCurves)
+	{
+		UAnimationBlueprintLibrary::RemoveAllCurveData(TargetSequence);
+	}
 
-  // copy all curves to the target anim sequence
-  for (auto& sourceCurve : SourceSequence->GetCurveData().FloatCurves)
-  {
-    // remove the curve if it already exists ==> overwrite
-    if (UAnimationBlueprintLibrary::DoesCurveExist(TargetSequence, sourceCurve.Name.DisplayName, ERawCurveTrackTypes::RCT_Float))
-    {
-      UAnimationBlueprintLibrary::RemoveCurve(TargetSequence, sourceCurve.Name.DisplayName);
-    }
+	// copy all curves to the target anim sequence
+	for (auto& sourceCurve : SourceSequence->GetCurveData().FloatCurves)
+	{
+		// remove the curve if it already exists ==> overwrite
+		if (UAnimationBlueprintLibrary::DoesCurveExist(TargetSequence, sourceCurve.GetName(), ERawCurveTrackTypes::RCT_Float))
+		{
+			UAnimationBlueprintLibrary::RemoveCurve(TargetSequence, sourceCurve.GetName());
+		}
 
-    // introduce the curve
-    UAnimationBlueprintLibrary::AddCurve(TargetSequence, sourceCurve.Name.DisplayName);
+		// introduce the curve
+		UAnimationBlueprintLibrary::AddCurve(TargetSequence, sourceCurve.GetName());
 
-    // prepare needed objects to set the curve keys
-    const FName containerName = UAnimationBlueprintLibrary::RetrieveContainerNameForCurve(TargetSequence, sourceCurve.Name.DisplayName);
-    if (containerName == NAME_None)
-    {
-      UE_LOG(LogTemp, Error, TEXT("Failed to get container name for curve %s aborting copy curves to %s"), *sourceCurve.Name.DisplayName.ToString(), *TargetSequence->GetName());
-      return;
-    }
+		// prepare needed objects to set the curve keys
+		const FAnimationCurveIdentifier curveId(sourceCurve.GetName(), ERawCurveTrackTypes::RCT_Float);
 
-    const FSmartName curveSmartName = UAnimationBlueprintLibrary::RetrieveSmartNameForCurve(TargetSequence, sourceCurve.Name.DisplayName, containerName);
-    const FAnimationCurveIdentifier curveId(curveSmartName, ERawCurveTrackTypes::RCT_Float);
-
-    // transfer curve keys
-    TargetSequence->GetController().SetCurveKeys(curveId, sourceCurve.FloatCurve.GetConstRefOfKeys());
-  }
+		// transfer curve keys
+		TargetSequence->GetController().SetCurveKeys(curveId, sourceCurve.FloatCurve.GetConstRefOfKeys());
+	}
 }
--- Source/TTToolbox/Private/TTPoseableMeshComponent.cpp
+++ Source/TTToolbox/Private/TTPoseableMeshComponent.cpp
@@ -22,39 +22,39 @@
 
 void UTTPoseableMeshComponent::SetBoneLocalTransformByName(const FName& BoneName, const FTransform& InTransform)
 {
-  if (!SkeletalMesh || !RequiredBones.IsValid())
-  {
-    return;
-  }
+	if (!GetSkinnedAsset() || !RequiredBones.IsValid())
+	{
+		return;
+	}
 
-  int32 boneIndex = GetBoneIndex(BoneName);
-  if (boneIndex >= 0 && boneIndex < BoneSpaceTransforms.Num())
-  {
-    BoneSpaceTransforms[boneIndex] = InTransform;
-    MarkRefreshTransformDirty();
-  }
+	int32 boneIndex = GetBoneIndex(BoneName);
+	if (boneIndex >= 0 && boneIndex < BoneSpaceTransforms.Num())
+	{
+		BoneSpaceTransforms[boneIndex] = InTransform;
+		MarkRefreshTransformDirty();
+	}
 }
 
 void UTTPoseableMeshComponent::UpdatePose()
 {
-  // Can't do anything without a SkeletalMesh
-  if (!SkeletalMesh)
-  {
-    return;
-  }
+	// Can't do anything without a SkeletalMesh
+	if (!GetSkinnedAsset())
+	{
+		return;
+	}
 
-  // Do nothing more if no bones in skeleton.
-  if (GetNumComponentSpaceTransforms() == 0)
-  {
-    return;
-  }
+	// Do nothing more if no bones in skeleton.
+	if (GetNumComponentSpaceTransforms() == 0)
+	{
+		return;
+	}
 
-  // We need the mesh space bone transforms now for renderer to get delta from ref pose:
-  FillComponentSpaceTransforms();
-  FinalizeBoneTransform();
+	// We need the mesh space bone transforms now for renderer to get delta from ref pose:
+	FillComponentSpaceTransforms();
+	FinalizeBoneTransform();
 
-  UpdateChildTransforms();
-  UpdateBounds();
-  MarkRenderTransformDirty();
-  MarkRenderDynamicDataDirty();
+	UpdateChildTransforms();
+	UpdateBounds();
+	MarkRenderTransformDirty();
+	MarkRenderDynamicDataDirty();
 }
